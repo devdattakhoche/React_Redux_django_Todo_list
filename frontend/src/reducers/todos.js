0e2bbe16aeb9be81b45c2a9cf47d8d9ca7bfad14
devdattakhoche <dkhoche70@gmail.com>
2020-03-13 10:57:32 +0530

    Modal.js added

--- frontend/src/reducers/todos.js
+++ frontend/src/reducers/todos.js
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import { GET_TODOS, ADD_TODO } from "../actions/types";
+import { GET_TODOS, ADD_TODO , DELETE_TODO , GET_TODO } from "../actions/types";
 
 export default (state = {}, action) => {
   switch (action.type) {
@@ -8,11 +8,14 @@ export default (state = {}, action) => {
         ...state,
         ..._.mapKeys(action.payload, "id")
       };
+    case GET_TODO:
     case ADD_TODO: // added
       return {
         ...state,
         [action.payload.id]: action.payload
       };
+      case DELETE_TODO: // added
+      return _.omit(state, action.payload);
     default:
       return state;
   }
